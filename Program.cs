5d01a4bac0d60fcab64a33a42b0cb35075a80782
Liam Armitage <34692095+PukekoKiwi@users.noreply.github.com>
2021-09-01 20:23:14 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace BikeLocker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            string line;
+            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\liamr\Downloads\words_alpha.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line.Length == 4)
+                {
+                    var wordChars = line.ToCharArray();
+                    if ("stdmrfblpw".Contains(wordChars[0]) && "wryuaioleh".Contains(wordChars[1]) && "koalermtns".Contains(wordChars[2]) && "kmstepyldg".Contains(wordChars[3]))
+                    {
+                        System.Console.WriteLine(line);
+                        counter++;
+                    }
+                }
+            }
+            file.Close();
+            System.Console.WriteLine("There were {0} valid words.", counter);
+        }
+    }
+}
